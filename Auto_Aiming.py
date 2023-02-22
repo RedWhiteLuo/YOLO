8873c82c3c07f348607f31a40672e255b1bdb5de
RedWhiteLuo <126067610+RedWhiteLuo@users.noreply.github.com>
2023-02-22 23:44:27 +0800

    Update Auto_Aiming.py

--- Auto_Aiming.py
+++ Auto_Aiming.py
@@ -40,6 +40,7 @@ import numpy as np
 import win32gui
 import win32ui
 import win32con
+
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
@@ -52,8 +53,9 @@ from utils.general import (LOGGER, Profile, check_file, check_img_size, check_im
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, smart_inference_mode
+
 # 以下为GRAB_screen的初始化参数
-xywh = [600, 180, 640, 512]
+xywh = [640, 284, 640, 512]
 hwin = win32gui.GetDesktopWindow()
 hwindc = win32gui.GetWindowDC(hwin)
 srcdc = win32ui.CreateDCFromHandle(hwindc)
@@ -63,12 +65,14 @@ bmp.CreateCompatibleBitmap(srcdc, xywh[2], xywh[3])
 memdc.SelectObject(bmp)
 
 # 此处需要手动输入长宽（用于MOUSE_move）
-screen_w, screen_h = 640, 500
+screen_w, screen_h = 640, 512
+# 关闭安全限制
+pydirectinput.PAUSE = 0.0
 
 
 def IMG_show(img_list):
     while True:
-        if img_list.full():     # 冗余，防止因其他原因使进程阻塞进而导致推理过程暂停
+        if img_list.full():  # 冗余，防止因其他原因使进程阻塞进而导致推理过程暂停
             for x in range(4):
                 img_list.get()
         cv2.imshow("THIS IS ASYNC RESULT", img_list.get())
@@ -77,16 +81,15 @@ def IMG_show(img_list):
 
 def MOUSE_move(data):
     while True:
+        if data.full():  # 冗余，防止因其他原因使进程阻塞进而导致推理过程暂停
+            for x in range(4):
+                data.get()
         choose_close = data.get()
-        if choose_close[1] != 0:    # 此处是用来防止归位后 x,y 都等于“0”的情况
+        if choose_close[1] != 0:  # 此处是用来防止归位后 x,y 都等于“0”的情况
             pydirectinput.moveRel(xOffset=int(choose_close[1] - screen_w / 2),
                                   yOffset=int(choose_close[2] - screen_h / 2), relative=True)
             print("ASYNC[x y mouse_move_x mouse_move_y ]", choose_close[0], choose_close[1], choose_close[2],
                   int(choose_close[1] - (screen_w / 2)), int(choose_close[2] - (screen_h / 2)))
-            # 判断是否在指定的范围内，如满足则点下左键
-            if choose_close[0] <= 200:
-                pydirectinput.click()
-                # time.sleep(1.5)
 
 
 def GRAB_screen():
@@ -100,6 +103,7 @@ def GRAB_screen():
     im = im.swapaxes(1, 2)  # 交换为[c,w,h]
     im[[0, 1, 2]] = im[[2, 1, 0]]  # 将RGB 转换为 BGR
     return im, im0s
+
 
 @smart_inference_mode()
 def run(
@@ -117,6 +121,7 @@ def run(
         view_img_async=False,  # show results in async
         move_mouse_sync=False,
         move_mouse_async=False,
+        shoot=False,
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
         augment=False,  # augmented inference
@@ -126,7 +131,6 @@ def run(
         dnn=False,  # use OpenCV DNN for ONNX inference
 ):
     source = str(source)
-
     # Load model
     device = select_device(device)
     model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
@@ -179,13 +183,13 @@ def run(
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    ppl_list = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()    # 返回的是坐标框中心的xywh
+                    ppl_list = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # 返回的是坐标框中心的xywh
                     # 因为没有训练专门模型，此处用来排除手的干扰
                     if ppl_list[0] > 0.7 and ppl_list[1] > 0.7:
                         continue
                     # 获得检测框的中心，以及该中心距离屏幕中心的距离
-                    ppl_x, ppl_y = int((ppl_list[0]) * screen_w), int((ppl_list[1] - ppl_list[3] * 0.25) * screen_h)
-                    ppl_distance = int(ppl_x - screen_w / 2) ** 2 + int(ppl_y - screen_h / 2) ** 2      # 用来判断与头的距离
+                    ppl_x, ppl_y = int((ppl_list[0]) * screen_w), int((ppl_list[1] - ppl_list[3]*0.25) * screen_h)
+                    ppl_distance = int(ppl_x - screen_w / 2) ** 2 + int(ppl_y - screen_h / 2) ** 2  # 用来判断与头的距离
                     # 用来保存在一张图片中距离中心点最近的坐标框
                     if ppl_distance < choose_close[0]:
                         choose_close[0], choose_close[1], choose_close[2] = ppl_distance, ppl_x, ppl_y
@@ -194,28 +198,30 @@ def run(
                         c = int(cls)  # integer class
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')  # 标签
                         annotator.box_label(xyxy, label, color=colors(int(conf), True))  # 坐标 标签 颜色（置信度）
-                    # 移动鼠标（自瞄部分，这一块需要使用管理员权限，并在游戏内打开，不然有可能会失效）
-                if move_mouse_async:    # 异步
+                # 移动鼠标（自瞄部分，这一块需要使用管理员权限，并在游戏内打开，不然有可能会失效）
+                if move_mouse_async:  # 异步
                     mouse_move.put(choose_close)
-                if move_mouse_sync:     # 同步
+                if move_mouse_sync:  # 同步
                     if choose_close[1] != 0:
                         pydirectinput.moveRel(xOffset=int(choose_close[1] - screen_w / 2),
-                                                  yOffset=int(choose_close[2] - screen_h / 2), relative=True)
+                                              yOffset=int(choose_close[2] - screen_h / 2), relative=True)
                         print("SYNC[x y mouse_move_x mouse_move_y ]", choose_close[0], choose_close[1], choose_close[2],
-                                int(choose_close[1] - (screen_w / 2)), int(choose_close[2] - (screen_h / 2)))
-                        if choose_close[0] <= 200:
-                            pydirectinput.click()
-                            # time.sleep(1.5)
+                              int(choose_close[1] - (screen_w / 2)), int(choose_close[2] - (screen_h / 2)))
+                if shoot:
+                    if choose_close[0] <= 200:
+                        pydirectinput.click()
+                        time.sleep(1.5)
                 choose_close = [10000000000, 0, 0]
             # Stream results
             if view_img_async:  # 异步
                 show_img.put(annotator.result())
-            if view_img_sync:   # 同步
+            if view_img_sync:  # 同步
                 cv2.imshow("THIS IS SYNC RESULT", annotator.result())
                 cv2.waitKey(1)  # 1 millisecond
 
         # Print time
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}inference: {dt[1].dt * 1E3:.1f}ms, total: {time_count* 1E3:.2f}ms")
+        LOGGER.info(
+            f"{s}{'' if len(det) else '(no detections), '}inference: {dt[1].dt * 1E3:.1f}ms, total: {time_count * 1E3:.2f}ms")
 
     # 由于是死循环所以这个选项没有用
     if update:
@@ -227,19 +233,20 @@ def parse_opt():
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
     parser.add_argument('--source', type=str, default=ROOT / 'screen', help='file/dir/URL/glob/screen/0(webcam)')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--img-size', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--img-size', '--img', '--img-size', nargs='+', type=int, default=[640],
+                        help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--max-det', type=int, default=3, help='maximum detections per image')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img-sync', default=False, action='store_true', help='view-img-sync')
     parser.add_argument('--view-img-async', default=False, action='store_true', help='view-img-async')
     parser.add_argument('--move-mouse-sync', default=False, action='store_true', help='move-mouse-sync')
     parser.add_argument('--move-mouse-async', default=False, action='store_true', help='move-mouse-async')
+    parser.add_argument('--shoot', default=False, action='store_true', help='shoot')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
     parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
@@ -258,14 +265,13 @@ def main(show_img_0, mouse_move_0, opt_0):
 if __name__ == "__main__":
     opt = parse_opt()
     show_img = Queue(5)
-    mouse_move = Queue()
+    mouse_move = Queue(5)
     MAIN = multiprocessing.Process(target=main, args=(show_img, mouse_move, opt))
     SHOW = multiprocessing.Process(target=IMG_show, args=(show_img,))
     MOVE = multiprocessing.Process(target=MOUSE_move, args=(mouse_move,))
     MAIN.start()
     SHOW.start()
     MOVE.start()
-
 
 '''
 测试数据（3060 Laptop，i7 12700h）：
